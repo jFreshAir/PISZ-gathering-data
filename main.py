8d5f0c834ca1150f256e4797951d30dd229079e3
Jurij Frešer <jurij.freser@student.um.si>
2022-11-08 19:28:40 +0100

    Finished the funciton for reading results, function for sorting, saving pickle file and writing to csv.

--- main.py
+++ main.py
@@ -1,31 +1,98 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
 # S pomočjo knjižnice sport-activities-features razčlenite podatke.
 
 import os
+import re
+import pickle
+from sport_activities_features.hill_identification import HillIdentification
+from sport_activities_features.tcx_manipulation import TCXFile
+from sport_activities_features.topographic_features import TopographicFeatures
+from sport_activities_features.plot_data import PlotData
+
 
 directory = 'C:\\FERI\\PISZ\\Sport5\\1'
+tcx_file = TCXFile()
 
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    if os.path.isfile(f):
-        print(f)
 
 # Pridobite vsaj integralne metrike, podatke o hribih in jih združite glede na aktivnost (vsak šport naj
 # ima eno izhodno datoteko glede na tip aktivnosti, ki združuje vse treninge z izbrano športno aktivnostji).
+
+all_results = []
+all_activities = []
+def SaveResultArrayPickle(result):
+
+    #preveri pravilnost oblike rezultata
+    if(type(result) is not dict):
+        print("The result must be a dictionary!")
+        return
+    #dodaja nove vrste aktivnosti
+    for type in all_activities:
+        if(result['activity_type'] == type):
+            continue
+        else:
+            all_activities.append(result['activity_type'])
+
+    #shrani rezultat v polje in datoteko
+    all_results.append(result)
+    #pickle
+    file = open('C:\FERI\PISZ\processedData.pickle', "wb")
+    pickle.dump(all_results, file)
+    file.close()
+
+def foo():
+    a = "tunekajpiše"
+    b = "to je naslednja vrstica"
+
+    f = open("C:\\FERI\\PISZ\\" + 'neke' + ".txt", 'a')
+    f.write(a + "\n")
+    f.write(b)
+
+def SortByType():
+    for result in all_results:
+        type = result['activity_type']
+        f = open('C:\\FERI\\PISZ\\SportDataByType\\' + type + '.csv', 'a')
+        f.write(result)
+        f.close()
+
+
+def ReadFiles():
+    try:
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+            if os.path.isfile(f):
+
+                id = os.path.splitext(filename)[0]
+                print(id)
+
+                integralMetrics = tcx_file.extract_integral_metrics(f)
+                #print(integralMetrics)
+
+                data = tcx_file.read_one_file(f)
+                #print(data.keys())
+                hill = HillIdentification(data['altitudes'])
+                hill.identify_hills()
+                allHills = hill.return_hills()
+                Top = TopographicFeatures(allHills)
+                num = Top.num_of_hills()
+                avg = Top.avg_altitude_of_hills(data['altitudes'])
+                avgAscent = Top.avg_ascent_of_hills(data['altitudes'])
+                distance_hills = Top.distance_of_hills(data['positions'])
+
+                single_result = {
+                    "id": id,
+                    "activity_type": data['activity_type'],
+                    "intregral_metrics": integralMetrics,
+                    "hill_data": Top
+                }
+
+                print(single_result)
+                SaveResultArrayPickle(single_result)
+    except:
+        print("Error, file " + filename)
+
+
+#ReadFiles()
+#foo()
+
 # Izhodna CSV datoteka naj vsebuje naslednje vsaj stolpce.
 # ime_aktivnosti, id_sportnika, integralne_metrike (total duration, total_distance, calories...),
 # podatke_o_hribih (num_hills, avg_altitude...)
